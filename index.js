f83550d75b2e555692f5a367e86f78ac0177159a
Lakshmi <lakshmi.nishthala@gmail.com>
2017-11-21 09:09:33 -0800

    Trying to do the routing

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const app = express();
 const redis = require('redis');
 const redisClient = redis.createClient();
 const shortener = require('./link_shortener');
+
+app.get('/', (req, res) => {
+	res.end('./index.html');
+});
+
+app.post('/', (req, res) => {
+	let url = req.body.original_url;
+	let short_url = shortener(url);
+	res.end(short_url);
+});
+
+app.listen(3000);
